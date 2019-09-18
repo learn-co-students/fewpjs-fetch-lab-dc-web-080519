3c42e22e1a834fb2673f6519460d87007e01415e
github username <github email address>
2019-09-18 12:11:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(json => renderBooks(json))
 }
+
+
+
+/*
+fetch('http://api.open-notify.org/astros.json')
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log(json);
+  console.log(`Holy cow! There are ${json["number"]} humans in space.`);
+});
+*/
+
+
+
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
